d78d2f2afdf81f76213f3b56bf7fd94dbb40a2a4
nepogoda <tatiana_deutsch@mail.ru>
2024-03-24 16:32:28 +0300

    create celery

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from sqlalchemy import create_engine, MetaData, NullPool
+from sqlalchemy import create_engine, MetaData
 from src.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 DATABASE_URL = (
